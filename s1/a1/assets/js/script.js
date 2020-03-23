200ef6e86da17552e60ade40e39428a1ff506e67
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-03-23 16:14:45 +0800

    this version will include activity 1

---
+++ s1/a1/assets/js/script.js
@@ -0,0 +1,7 @@
+let firstName = "Arwyne";
+let lastName = "De Guzman";
+let company = "Zuitt";
+
+console.log('"Hi ' + firstName + " " + lastName + "! Welcome to " + company + '!"')
+
+console.log(`"Hi ${firstName} ${lastName}! Welcome to ${company}!"`)
