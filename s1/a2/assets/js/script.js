5a3a9926298cf05c291c1cb877b37a34236ce183
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-03-24 08:42:02 +0800

    this version includes activity2 of s1

---
+++ s1/a2/assets/js/script.js
@@ -0,0 +1,17 @@
+let firstVal = 55;
+let secondVal = 10;
+
+firstVal += 5;
+console.log(firstVal);
+
+firstVal = firstVal - secondVal;
+console.log("the difference between the 2 values is " + firstVal);
+
+secondVal += 20
+console.log("this is the value of secondVal: " + secondVal)
+
+let thirdVal = firstVal / secondVal;
+console.log("this is the value of thirdVal: " + thirdVal);
+console.log(firstVal % thirdVal);
+
+alert("time to submit to BDLE!");
