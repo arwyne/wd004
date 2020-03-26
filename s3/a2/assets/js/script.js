23ad5f2ecabd82b63e465bd0e75af1e5fe49123d
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-03-26 08:29:46 +0800

    make changes on activity 2

--- s3/a2/assets/js/script.js
+++ s3/a2/assets/js/script.js
@@ -2,13 +2,25 @@ let evenSum = 0;
 let oddSum = 0;
 let x;
 
-for(x = 0; x <= 50; x += 2) {
-	evenSum += x
+for(x = 0; x <= 50 ; x++) {
+	if(x%2 == 0) {
+		evenSum += x;
+	} else {
+		oddSum += x;
+	}
 }
+
 console.log('the sum of all even numbers from 0-50 is ' + evenSum)
-
-for(x = 1; x <= 50; x += 2 ) { 
-	oddSum += x
-}
-
 console.log('the sum of all odd numbers from 0-50 is ' + oddSum)
+
+
+// for(x = 0; x <= 50; x += 2) {
+// 	evenSum += x
+// }
+// console.log('the sum of all even numbers from 0-50 is ' + evenSum)
+
+// for(x = 1; x <= 50; x += 2 ) { 
+// 	oddSum += x
+// }
+
+// console.log('the sum of all odd numbers from 0-50 is ' + oddSum)
--- s3/d1/assets/js/script.js
+++ s3/d1/assets/js/script.js
@@ -114,3 +114,123 @@ switch(diceRoll) {
 		console.log('your roll is less than 4');
 }
 
+// multiple values for switch-case statement
+// highly suggest to use if-else instead
+let testx = 1;
+let testy = 2;
+
+// if you want your switch-case statement to run no matter what
+// pass true as the expression being checked by the switch-case
+switch(true) {
+	case testx == 1 && testy == 2:
+		console.log('&& is satisfied');
+		break;
+	case testx == 1 || testy == 1:
+		console.log('|| is satisfied');
+		break;
+	default:
+		console.log('default statement ran');
+		break;
+}
+
+
+// loops
+	// used to repeat pieces of code over and over according conditions
+
+	// display the number of tasks remaining for a user until 1 task is left
+	// if value of tasks < 1, do not output anything in the console
+	let tasks = 100;
+	// there are 5 tasks left
+	// console.log('there are 5 tasks left');
+	// console.log('there are 4 tasks left');
+	// console.log('there are 3 tasks left');
+	// console.log('there are 2 tasks left');
+	// console.log('there is 1 task left');
+
+
+	// while loop
+	/*
+		while(condition is true/being fulfilled) {
+			code block to be run if condition is true
+		}
+	*/
+
+	while(tasks > 0) {
+		// while the value of tasks is greater than 0, execute this code
+		// code block will run as long as value of tasks is greater than 0
+		// decrementing the value of tasks means that the next iteration
+		// of the loop will give the tasks variable a value of current value-1
+
+		// console.log('there are ' + tasks + ' tasks left'); 
+
+		if(tasks > 1){
+			console.log('there are ' + tasks + " tasks left");
+		} else {
+			console.log('there is ' + tasks + " task left");
+		}
+
+		tasks-- 
+
+	}
+
+
+	// 	while (tasks > 0) {
+	//     	console.log(`there are ${tasks} tasks left`)
+	//     	tasks--
+	//     	if (tasks == 1) {
+	//         console.log(`there is ${tasks} task left`)
+	//         break
+	//     }
+	// }
+
+	
+	// infinite loop
+	// it is important that the given condition in the while loop
+	// will be met and BREAK AT SOME POINT
+	// if not, the loop will be infinite; may cause our program to crash
+
+	// let inf = 1;
+
+	// while(inf > 0) {
+	// 	console.log('PRINT ME!')
+	// 	inf++;
+	// }
+
+
+	// do-while loop
+	// similar to a while loop. only difference is the condition is checked
+	// after running the code once
+
+	/*
+		do {
+			code to be run
+		} while(condition to be checked)
+
+	*/
+
+	let tasksDo = 3
+	do {
+		// code block will run atleast once even if the condition is not met
+		console.log('mayroon ' + tasksDo + " tasks left")
+	} while(tasksDo > 5) // checks if condition is met. if not, loop will terminate
+
+
+
+
+	// for loop
+	// syntax:
+	// for(initialization value; condition to be checked; increment/decrement after) {
+	//		code block to be run if condition is met
+	// }
+
+	let tasksFor;
+
+	for(tasksFor = 6; tasksFor < 10; tasksFor++) {
+		console.log('you still have ' + tasksFor + " tasks left");
+		//console.log('you still have ' + 6 + " tasks left");
+		//console.log('you still have ' + 7 + " tasks left");
+		//console.log('you still have ' + 8 + " tasks left");
+		//console.log('you still have ' + 9 + " tasks left");
+	}
+
+
