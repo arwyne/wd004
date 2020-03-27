de6b5ae5a04f41f2ded1074feda7cce8b822b18e
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-03-27 15:48:27 +0800

    this version include solution 2 on activity1 s4

--- s4/a1/assets/js/script.js
+++ s4/a1/assets/js/script.js
@@ -5,117 +5,158 @@ let dayInput = 23;
 
 const months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-const zodiac = ['Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagitarius', 'Capricorn']
+const zodiac = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn']
 
+
+// solution 2
 
 switch(monthInput) {
+
+	// jan 1-19 capricorn[0]
+	// jan 20 onwards aquarius zodiac[1]
 	case 1:
 		if(dayInput >= 20) {
-			console.log(months[1] + " " + dayInput + " is " + zodiac[0])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
-		else{
-			console.log(months[1] + " " + dayInput + " is " + zodiac[11])
-		} break
+		else {
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// feb 1-18 aquarius zodiac[1]
+	// feb 19 onwards zodiac[2]
 	case 2:
 		if(dayInput >= 19) {
-			console.log(months[2] + " " + dayInput + " is " + zodiac[1])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[2] + " " + dayInput + " is " + zodiac[0])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// march 1-20 pisces zodiac[2]
+	// march 21 onwards aries zodiac[3]
 	case 3:
 		if(dayInput >= 21) {
-			console.log(months[3] + " " + dayInput + " is " + zodiac[2])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[3] + " " + dayInput + " is " + zodiac[1])
-		}break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// april 1-19 aries zodiac[3]
+	// april 20 onwards taurus zodiac[4]
 	case 4:
 		if(dayInput >= 20) {
-			console.log(months[4] + " " + dayInput + " is " + zodiac[3])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[4] + " " + dayInput + " is " + zodiac[2])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// may 1 - 20 taurus zodiac[4]
+	// may 21 onwards gemini zodiac[5]
 	case 5:
 		if(dayInput >= 21) {
-			console.log(months[5] + " " + dayInput + " is " + zodiac[4])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[5] + " " + dayInput + " is " + zodiac[3])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// june 1-20 gemini zodiac[5]
+	// june 21 onwards cancer zodiac[6]
 	case 6:
 		if(dayInput >= 21) {
-			console.log(months[6] + " " + dayInput + " is " + zodiac[5])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[6] + " " + dayInput + " is " + zodiac[4])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// july 1-22 cancer zodiac[6]
+	// july 23 onwards leo zodiac[7]
 	case 7:
 		if(dayInput >= 23) {
-			console.log(months[7] + " " + dayInput + " is " + zodiac[6])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[7] + " " + dayInput + " is " + zodiac[5])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		} 
+		break
 
 
+	// august 1-22 leo zodiac[7]
+	// august 23 onwards virgo zodiac [8]
 	case 8:
 		if(dayInput >= 23) {
-			console.log(months[8] + " " + dayInput + " is " + zodiac[7])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[8] + " " + dayInput + " is " + zodiac[6])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// september 1-22 virgo zodiac[8]
+	// september 23 onwards libra zodiac[9]
 	case 9:
 		if(dayInput >= 23) {
-			console.log(months[9] + " " + dayInput + " is " + zodiac[8])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[9] + " " + dayInput + " is " + zodiac[7])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// october 1-22 libra zodiac[9]
+	// october 23 onwards scorpio zodiac[10]
 	case 10:
 		if(dayInput >= 23) {
-			console.log(months[10] + " " + dayInput + " is " + zodiac[9])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[10] + " " + dayInput + " is " + zodiac[8])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// november 1-21 scorpio zodiac[10]
+	// november 22 onwards sagittarius zodiac[11]
 	case 11:
 		if(dayInput >= 22) {
-			console.log(months[11] + " " + dayInput + " is " + zodiac[10])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[11] + " " + dayInput + " is " + zodiac[9])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 
+	// december 1-21 sagittarius zodiac[11]
+	// december 22 onwards capricorn zodiac[12]
 	case 12:
 		if(dayInput >= 22) {
-			console.log(months[12] + " " + dayInput + " is " + zodiac[11])
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput])
 		}
 		else{
-			console.log(months[12] + " " + dayInput + " is " + zodiac[10])
-		} break
+			console.log(months[monthInput] + " " + dayInput + " is " + zodiac[monthInput-1])
+		}
+		break
 
 	default:
-		console.log("INPUT A CORRECT BIRTHDAY!!")
+		console.log("INPUT A CORRECT MONTH!!")
 }
+
+
